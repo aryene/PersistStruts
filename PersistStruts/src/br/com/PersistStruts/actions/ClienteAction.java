8f0164b7baa99d70ff695df9be799f4d40f562f5
daniel <daniel@not-gardino>
2016-08-31 08:13:54 -0300

    Struts+JPA

---
+++ PersistStruts/src/br/com/PersistStruts/actions/ClienteAction.java
@@ -0,0 +1,40 @@
+package br.com.PersistStruts.actions;
+
+
+import javax.inject.Inject;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.opensymphony.xwork2.ActionSupport;
+
+import br.com.PersistStruts.modelo.Cliente;
+import br.com.PersistStruts.servicos.ClienteServico;
+
+public class ClienteAction extends ActionSupport {
+	
+	private static final long serialVersionUID = 1L;
+	
+	private Cliente cliente;
+	
+	@Autowired
+	ClienteServico clienteServico;
+
+	public String cadastraCliente(){
+		this.clienteServico.salvar(this.getCliente());
+		System.out.println("passou");
+		return SUCCESS;
+	}
+
+	public Cliente getCliente() {
+		return cliente;
+	}
+
+	public void setCliente(Cliente cliente) {
+		this.cliente = cliente;
+	}
+
+
+	
+	
+
+}
