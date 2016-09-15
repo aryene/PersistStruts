fec4590eed9ee31ce826d8c76e300921bf1a0bf4
daniel <daniel@not-gardino>
2016-09-14 21:10:16 -0300

    Agora com TILES

--- PersistStruts/src/br/com/PersistStruts/dao/FornecedorDao.java
+++ PersistStruts/src/br/com/PersistStruts/dao/FornecedorDao.java
@@ -3,10 +3,13 @@ package br.com.PersistStruts.dao;
 import org.springframework.stereotype.Repository;
 
 import br.com.PersistStruts.modelo.Cliente;
+import br.com.PersistStruts.modelo.Fornecedor;
 
 
 @Repository
-public class ClienteDao extends GenericDao<Cliente, Integer> {
+public class FornecedorDao extends GenericDao<Fornecedor, Integer> {
+
+	
 		
 	
 }
